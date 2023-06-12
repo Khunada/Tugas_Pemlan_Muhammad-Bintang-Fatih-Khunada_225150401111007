a2eebdce33e9cc7cbe4bb9e423c4215181c95fc1
Khunada <136248672+Khunada@users.noreply.github.com>
2023-06-12 08:55:39 +0700

    Vehicle.java

---
+++ GitHub/src/Tugas_8_AbstractClass_Method/Vehicle.java
@@ -0,0 +1,20 @@
+package Tugas_8_AbstractClass_Method;
+
+public abstract class Vehicle {
+    private String name;
+    private int year;
+    private double rpm;
+    Vehicle(String name, int year, double rpm) {
+    this.name = name;
+    this.year = year;
+    this.rpm = rpm;
+    }
+    public String getName() {
+    return name;
+    }
+    public int getYear() {
+    return year;
+    }
+    public double getRpm() {
+    return rpm;
+    } }
