caba19315dc1cefe8534b8f5efce31b0209b2b6d
Khunada <136248672+Khunada@users.noreply.github.com>
2023-06-12 08:55:12 +0700

    Owner.java

---
+++ GitHub/src/Tugas_8_AbstractClass_Method/Owner.java
@@ -0,0 +1,10 @@
+package Tugas_8_AbstractClass_Method;
+
+public class Owner {
+    private String name;
+    public Owner(String name) {
+    this.name = name;
+    }
+    public String getName() {
+    return name;
+    } }
